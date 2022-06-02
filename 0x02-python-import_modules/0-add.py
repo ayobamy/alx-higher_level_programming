e184cfbe04695744360792b92dbdada7002bb4a6
ayobamy <ayobamyahmed@gmail.com>
2022-06-02 13:12:51 -0700

    modified: added guards

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":
+    """Guarded my script"""
 
-a = 1
-b = 2
+    from add_0 import add
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
