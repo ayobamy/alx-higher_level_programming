d52174a8b6959fa4e3159b7b2557d05af090b261
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-06-06 08:32:45 +0000

    retrives a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if (idx < 0 and idx > len(my_list)):
+        return None
+    for m in my_list:
+        if m == idx:
+            return my_list[m]
