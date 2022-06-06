55a8fb77a95a1f8cf7217d68896c443d1d38003a
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-06-06 08:14:20 +0000

    printed a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    a function that prints all integers of a list
+    Format: one integer per line
+
+    """
+    for ls in my_list:
+        print("{}".format(ls))
