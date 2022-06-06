6a60f045147391fb00aa1cad7d7d7537cd04044e
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-06-06 14:18:30 +0000

    modified

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if (idx < 0 and idx >= len(my_list)):
-        return mylist
-    """
-    creating a shallow copy
-    of the list
-    """
+    if (idx >= len(my_list) or idx < 0):
+        return my_list
     new = my_list[:]
     new[idx] = element
     return new
