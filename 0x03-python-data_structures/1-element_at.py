c59c7c840597a76ac8cdf018fbf7d90e7fc43776
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-06-06 08:36:50 +0000

    modified

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 def element_at(my_list, idx):
     if (idx < 0 and idx >= len(my_list)):
         return None
-    for m in my_list:
+    for m in range(len(my_list)):
         if m == idx:
             return my_list[m]
