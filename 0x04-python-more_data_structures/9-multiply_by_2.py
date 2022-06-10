0131c7af636c0244ad9b589b907aed733a21e0ac
ayobamy <ayobamyahmed@gmail.com>
2022-06-10 00:31:14 -0700

    modified

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,8 +5,8 @@ def multiply_by_2(a_dictionary):
     a function that returns a new dictionary
     with all values multiplied by 2
     """
-    new_values = {}
-    for v in a_dictionary:
-        new_dictionary[v] = (a_dictionary[v] * 2)
+    multiply_dictionary = a_dictionary.copy()
+    for k, v in multiply_dictionary.items():
+        multiply_dictionary[k] = v * 2
 
-    return new_dictionary
+    return multiply_dictionary
