1e7299434494db69cfc688f032700973fc175433
ayobamy <ayobamyahmed@gmail.com>
2022-06-20 20:24:03 +0000

    fixed bug

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-a function that prints an integer with "{:d}".format()
-"""
 
 
 def safe_print_integer(value):
     try:
-        int_val = int(value)
-        print("{:d}".format(int_val))
+        int(value)
+        print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
         return False
