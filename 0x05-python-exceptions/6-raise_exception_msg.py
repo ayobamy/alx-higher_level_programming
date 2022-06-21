f39e23c0be8e48340f90cd1780d4308c2c01484b
ayobamy <ayobamyahmed@gmail.com>
2022-06-21 12:44:48 +0000

    NameError Exception Handling

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that raises a name
+exception with a message.
+"""
+
+
+def raise_exception_msg(message=""):
+    if NameError:
+        raise NameError(message)
+    else:
+        pass
