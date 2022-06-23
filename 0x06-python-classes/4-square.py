4b2150e1f2e76a73c0ef68ed3ee733ed90fadbed
ayobamy <ayobamyahmed@gmail.com>
2022-06-23 03:55:53 +0000

    square class with property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Defines a square."""
+
+
+class Square:
+    """Representation of Square class"""
+
+    def __init__(self, size=0):
+        """Initialize a square
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Finds area of the square."""
+        return self.__size * self.__size
