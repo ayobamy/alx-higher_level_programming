0176a689c95eabf8d82922ec7d9e674cd0b85396
ayobamy <ayobamyahmed@gmail.com>
2022-06-27 21:26:32 +0000

    set and get value of rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,34 +12,34 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """Initializes the class"""
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """Retrieves the width"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """Sets the value of width"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if (value < 0):
             raise ValueError("width must be >= 0")
         else:
-            self._width = value
+            self.__width = value
 
     @property
     def height(self):
         """Retrieves the height"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """Sets the value of height"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if (value < 0):
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
