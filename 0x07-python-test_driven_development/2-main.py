72f5051a6bebe134d96b3a5b3150113d3ac1cf60
ayobamy <ayobamyahmed@gmail.com>
2022-06-29 01:53:36 +0000

    a function that prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/2-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 12]
+]
+print(matrix_divided(matrix, 3))
+print(matrix)
+# item = 12
+# num_row = 4
+
+# 12 / 4 = 3
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Divides all elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix by a given number.
+
+        matrix (list): A list of lists
+        div (int/float): The number of elements to divide each
+    Returns:
+        matrix: A list of new lists
+    """
+    if not isinstance(matrix, list):
+        raise TypeError(
+            'matrix must be a matrix (list of lists) of integers/floats')
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(
+                'matrix must be a matrix (list of lists) of integers/floats')
+        items_in_first_row = len(matrix[0])
+        if len(row) != items_in_first_row:
+            raise TypeError("Each row of the matrix must have the same size")
+        for items in row:
+            if not isinstance(items, (int, float)):
+                raise TypeError(
+                    'matrix must be a matrix (list of lists) of integers/floats')
+
+    return [list(map(lambda item: round((item / div), 2), row)) for row in matrix]
