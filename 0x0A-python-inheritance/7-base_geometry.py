07625b7851e2bcbd77989dceea5a80055c4a6c73
ayobamy <ayobamyahmed@gmail.com>
2022-07-05 21:16:37 +0000

    fixed bug

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -24,8 +24,7 @@ class BaseGeometry:
         if value is less or equal to 0: raise a ValueError exception
         with the message <name> must be greater than 0
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-
-        if value < 1:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
