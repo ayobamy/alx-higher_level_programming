40493ac29b2f760baa76ac732cbb9bb24f6522d0
ayobamy <ayobamyahmed@gmail.com>
2022-07-05 21:41:45 +0000

    fixed errors

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,53 +1,23 @@
 #!/usr/bin/python3
 """
-a definition of a class BaseGeometry
+Defines a class Rectangle that inherits from BaseGeometry
 """
-
-
-class BaseGeometry:
-    """A class definition"""
-
-    def area(self):
-        """
-        raises an Exception with the message
-        area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        validates value:
-
-        if value is not an integer: raise a TypeError exception,
-        with the message <name> must be an integer
-
-        if value is less or equal to 0: raise a ValueError exception
-        with the message <name> must be greater than 0
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""
-A subclass of class BaseGeometry
-"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
-    a subclass of the Super class BaseGeometry
+    a rectangle class inherited from BaseGeometry
     """
 
     def __init__(self, width, height):
         """
-        Instantiation with width and height:
-        width and height must be private. No getter or setter
-        width and height must be positive integers,
-        validated by integer_validator
+        Intializing a new Rectangle.
+        Args:
+            width (int): width of the Rectangle.
+            height (int): height of the Rectangle.
         """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
-        self.__height = width
+        self.integer_validator("height", height)
+        self.__height = height
