78494a921160d55543def803ebb99a3b2e10fa8c
ayobamy <ayobamyahmed@gmail.com>
2022-07-06 00:04:42 +0000

    JSON rep

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ a function that returns the JSON
+ representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+    JSON representation of an object
+    """
+    json_rep = json.dumps(my_obj)
+    return json_rep
