0c4d8d3f91f5af5baab7b8acdf23b76c9d397c04
ayobamy <ayobamyahmed@gmail.com>
2022-07-08 15:10:17 +0000

    base file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a module for base class
+"""
+
+
+class Base:
+    """
+    Defining a base class module
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        a constructor function
+        """
+        if id is not None:
+            self.id = id
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
