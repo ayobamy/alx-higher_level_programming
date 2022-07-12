99f2d7d4f5d48e85da126585c0b534c3f58422dc
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-07-12 20:40:37 +0000

    updated base class with JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@ a module for base class
 """
 
 
+import json
+
 class Base:
     """
     Defining a base class module
@@ -19,3 +21,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        convert list of dictionaries to json string
+        :param list_dictionaries: list of dictionaries
+        :return: json string
+        """
+        if (list_dictionaries is None or list_dictionaries == []):
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
