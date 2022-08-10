f17c8339daf0f0eed1393aaa6410e96fa1bcc6d5
ayobamy <ayobamyahmed@gmail.com>
2022-08-09 17:35:52 -0700

    [feat]: top 3 citiies

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- a script that displays the top 3 of cities temperature during July and August (DESC)
+SELECT city,
+AVG(value) AS 'avg_temp'
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
