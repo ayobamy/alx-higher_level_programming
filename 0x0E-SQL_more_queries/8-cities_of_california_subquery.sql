c3cc60a9da799919eb4a3cdf9aff2a4267738356
ayobamy <ayobamyahmed@gmail.com>
2022-08-10 18:37:39 -0700

    [feat]: a script that lists all the cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- a script that lists all the cities of California that can be found in the database hbtn_0d_usa
+-- The states table contains only one record where name = California
+-- Results must be sorted in ascending order by cities.id
+-- You are not allowed to use the JOIN keyword
+SELECT id, name FROM cities
+WHERE state_id IN (
+	SELECT id FROM states WHERE name='California'
+)
+ORDER BY id;
