6d58d0eb53dc2fa7fd0fcc9099fdf4c8abd7fcbf
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-07 00:13:14 +0000

    [feat]: a script that concats 2 files.

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv
+
+const fileOne = fs.readFileSync(args[2], 'utf8');
+const fileTwo = fs.readFileSync(args[3], 'utf8');
+
+fs.writeFileSync(args[4], fileOne + fileTwo);
