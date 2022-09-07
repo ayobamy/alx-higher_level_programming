0e4bdba5f35928c6e15618c4e35f1b1f5548a2fb
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-07 00:20:28 +0000

    [update]: changed path to string using toString prototype

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const args = process.argv;
 
-const fileOne = fs.readFileSync(args[2], 'utf8').toString;
-const fileTwo = fs.readFileSync(args[3], 'utf8').toString;
+const fileOne = fs.readFileSync(process.argv[2], 'utf8').toString;
+const fileTwo = fs.readFileSync(process.argv[3], 'utf8').toString;
 
-fs.writeFileSync(args[4], fileOne + fileTwo);
+fs.writeFileSync(process.argv[4], fileOne + fileTwo);
