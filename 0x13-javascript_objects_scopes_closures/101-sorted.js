5be8095db99b50d8e2ed413a81fe6688ebba9b55
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-07 00:00:05 +0000

    [feat]: script that imports a dictionary of occurrences by user id

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const newDictOccurence = {};
+
+for (const [key, value] of Object.entries(dict)) {
+  if (newDictOccurence[value]) {
+    newDictOccurence[value].push(key);
+  }
+  else {
+    (newDictOccurence[value] = [key]);
+  }
+}
+
+console.log(newDictOccurence);
