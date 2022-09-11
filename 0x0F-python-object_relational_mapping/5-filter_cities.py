1559ed0910f867f1005bcb19bde77da3f5788959
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-11 20:33:10 +0000

    [fix]: trailing whitespace

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
-    
+
     arg = sys.argv[4]
     cur = db.cursor()
     cur.execute('SELECT cities.name \
