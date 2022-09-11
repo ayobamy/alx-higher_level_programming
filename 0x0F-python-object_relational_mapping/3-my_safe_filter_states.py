d0074ee25e6524aaf2e47656b0baf7dc9837acdf
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-11 20:11:35 +0000

    [feat]: safe print with MySQL Injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a script that takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa where name matches
+the argument. But this time, write one that is safe from
+MySQL injections!
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    arg = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                   {'name': arg})
+
+    states = cur.fetchall()
+
+    for state in states:
+            print(state)
+
+    cur.close()
+    db.close()
