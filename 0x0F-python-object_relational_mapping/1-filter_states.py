818e404be96605a552de69d85a1f6a1c767f99b6
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-11 19:50:45 +0000

    [feat]: lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute(SELECT * FROM `states` ORDER BY `id`")
+    cur.execute(SELECT * FROM states ORDER BY id")
 
     states = cur.fetchall()
 
