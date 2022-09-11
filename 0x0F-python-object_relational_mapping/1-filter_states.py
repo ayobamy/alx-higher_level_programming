8f74c08baad8d10790040cd1c6ba2d11da9eab06
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-11 19:49:28 +0000

    [feat]: lists all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute(SELECT * FROM `states` ORDER BY `id`")
+
+    states = cur.fetchall()
+
+    for state in states:
+        if state[1][0] == "N":
+            print(state)
+
+    cur.close()
+    db.close()
