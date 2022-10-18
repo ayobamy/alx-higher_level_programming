71593d2afec72ccda411c9f8bc44adf11586db49
ayobamy <ayobamyahmed@gmail.com>
2022-10-18 16:39:20 -0700

    [feat]: starwars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
