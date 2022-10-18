1b969aa21d685340b7dea2b02b55f604f78569d9
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-18 20:28:36 +0000

    [update]: reads a file using fs module

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,3 +3,10 @@
 const require = require('fs');
 const fName = process.argv[2];
 
+fs.readFile(fName, 'utf-8', function(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
